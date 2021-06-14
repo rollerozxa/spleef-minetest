897ccee21d6ac9691f04637e1d9e941f02fad638
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-06-14 23:00:08 +0200

    HUD fixes.

--- mods/spleef/hand.lua
+++ mods/spleef/hand.lua
@@ -4,14 +4,13 @@ minetest.register_item(':', {
 	wield_scale = {x = 0.5, y = 1, z = 4},
 	range = 5,
 	tool_capabilities = {
-		full_punch_interval = 0.9,
+		full_punch_interval = 1,
 		max_drop_level = 0,
 		groupcaps = {
 			oddly_breakable_by_hand = {
-				times = {[1] = 3.50, [2] = 2.00, [3] = 0.20},
+				times = {[1] = 3.50, [2] = 2.00, [3] = 0.05},
 				uses = 0,
 			},
-		},
-		damage_groups = {fleshy = 1},
+		}
 	}
 })
